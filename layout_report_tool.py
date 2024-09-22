eacbaaa1917ba981ca15df4675abe2a9039dc806
ngotruongvu <119319477+ngovu123@users.noreply.github.com>
2024-09-22 15:02:00 +0700

    Update layout_report_tool.py

--- layout_report_tool.py
+++ layout_report_tool.py
@@ -12,7 +12,7 @@ def list_placeholders(design_number: int) -> pd.DataFrame:
     pd.DataFrame: DataFrame containing placeholder details.
     """
     placeholders_data = []
-    file_path = f"vietsub/Designs/Design-{design_number}.pptx"
+    file_path = f"Designs/Design-{design_number}.pptx"
     
     try:
         prs = Presentation(file_path)
